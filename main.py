f24cefc547df4a91ae28735f99eb22b47642afa0
Tatsuya Ishikawa <ishikawa11457@gmail.com>
2020-08-10 13:57:14 +0900

    pythonファイルをコンパイルした

---
+++ main.py
@@ -0,0 +1,6 @@
+from wrapper_hand_tracking_pb2 import WrapperHandTracking
+
+wrapperHandTracking = WrapperHandTracking()
+wrapperHandTracking.landmarks = 1
+wrapperHandTracking.rect = 2
+wrapperHandTracking.detection = 3
