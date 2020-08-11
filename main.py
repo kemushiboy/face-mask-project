050109a236653eb5ed993bb7961b38eb825f6c3f
Tatsuya Ishikawa <ishikawa11457@gmail.com>
2020-08-11 10:17:54 +0900

    UDP受信用のpythonプログラムを追加

--- main.py
+++
@@ -1,6 +0,0 @@
-from wrapper_hand_tracking_pb2 import WrapperHandTracking
-
-wrapperHandTracking = WrapperHandTracking()
-wrapperHandTracking.landmarks = 1
-wrapperHandTracking.rect = 2
-wrapperHandTracking.detection = 3
