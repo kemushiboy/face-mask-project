e0258114c63ea38c5dbb76ea7c01297fb8e1d89e
Tatsuya Ishikawa <ishikawa11457@gmail.com>
2020-08-11 10:53:46 +0900

    landmarksのdecodeに成功した

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import socket
 import time
+import wrapper_hand_tracking_pb2
 
 M_SIZE = 1024
-
 # 
 host = '127.0.0.1'
 port = 8080
@@ -16,20 +16,37 @@ print('create socket')
 # ②自ホストで使用するIPアドレスとポート番号を指定
 sock.bind(locaddr)
 
+
+def ListHandTracking(wrapperHandTracking):
+
+    #NormalizedLandmarkList landmarks = 1;
+    #optional NormalizedRect rect = 2;
+    #optional DetectionList detection = 3;
+
+    #for landmark in wrapperHandTracking.landmarks:
+    print ("Landmarks: ",wrapperHandTracking.landmarks)
+
+
 while True:
     try :
         # ③Clientからのmessageの受付開始
         print('Waiting message')
         message, cli_addr = sock.recvfrom(M_SIZE)
         #message = message.decode(encoding='utf-8')
-        print(f'Received message is [{message}]')
+
+        #*todo ここでmessageをprotobufのWrapperHandTracking形式でdecodeする
+        wrapper_hand_tracking = wrapper_hand_tracking_pb2.WrapperHandTracking()
+        wrapper_hand_tracking.ParseFromString(message)
+        ListHandTracking(wrapper_hand_tracking)
+
+        #print(f'Received message is [{message}]')
 
         # Clientが受信待ちになるまで待つため
-        time.sleep(1)
+        #time.sleep(1)
 
         # ④Clientへ受信完了messageを送信
-        print('Send response to Client')
-        sock.sendto('Success to receive message'.encode(encoding='utf-8'), cli_addr)
+        #print('Send response to Client')
+        #sock.sendto('Success to receive message'.encode(encoding='utf-8'), cli_addr)
 
     except KeyboardInterrupt:
         print ('\n . . .\n')
